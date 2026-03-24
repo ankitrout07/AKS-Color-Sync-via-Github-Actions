4371e3585e0cbe0a9c9f6c01dfe1da1b5adce751
ankitrout07 <ankitanupam3@gmail.com>
2026-03-24 16:04:58 +0530

    Ui-UpGRADE

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.use(express.static('.'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__current_dir, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
